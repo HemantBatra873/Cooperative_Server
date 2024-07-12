b861da6b5b2093abc9f6cac87bdefee433eafcc7
HemantBatra873 <domspidy050204@gmail.com>
2024-07-12 10:11:08 +0530

    second version

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
-import appRouter from "./routes/index.js";
+import appRouter from "./src/routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 config();
